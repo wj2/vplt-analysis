970bedd7b28563b6b7b395371a8475cd85e7f23c
U-FREEDMANLAB\wjj <wjj@Freedmanalysis2.freedmanlab.uchicago.edu>
2015-11-10 23:47:05 -0600

    update to fitting params to use

--- saccmcmc.py
+++ saccmcmc.py
@@ -2,10 +2,10 @@ import saccfit as sf
 import pymc
 
 if __name__ == '__main__':
-    iters = 60000
-    burn = 10000
-    thin = 5
-    save_int = 10000
+    iters = 20000
+    burn = 4000
+    thin = 4
+    save_int = 1000
     db = 'pickle'
     dbfile = 'saccade_fit.pkl'
     
