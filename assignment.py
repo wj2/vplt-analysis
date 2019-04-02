7706e67b8343159d7961d6d9ad647e7006da9a65
wjj <williamjjohnston@gmail.com>
2019-04-02 16:24:59 -0500

    Added new RF population creation function
    Started working on code to try to get around some issues with decoding
    -- decoding from populations with correlated noise is especially
    producing errors once bounds are introduced

--- assignment.py
+++ assignment.py
@@ -7,6 +7,7 @@ import scipy.integrate as sin
 import scipy.optimize as sio
 
 import general.utility as u
+import general.rf_models as rfm
 import mixedselectivity_theory.nms_continuous as nmc
 
 def simulate_estimates(p, s, pop_ests):
@@ -335,53 +336,96 @@ def minimum_weighted_error_lambda(bits, features, objs, p=100, lam_range=None,
     min_total = total[o_i, d1_i, range(len(lam_range))]
     return opt_d1, opt_over, min_total, lam_range, local_d, ae, total
 
-def estimate_encoding_swap(snr, rf_size, noise_var, noise_corr, dists,
-                           noise_samps=1000, buff=None, n=None, buff_mult=4,
-                           reses=None, rf_tiling=None, c=1, o=1, excl=True,
-                           dist_samps=10000, p_measure=np.median, neurs=None,
-                           filt_func=nmc.pure_filter, power_metric='variance',
-                           samps=1000, distortion=nmc.mse_distortion,
+def generate_1d_rfs(space_size, rf_spacing, rf_size, mult=1):
+    rf_cents = np.arange(0, space_size + rf_spacing, rf_spacing)
+    func = rfm.make_gaussian_vector_rf(rf_cents, rf_size, 1, 0)
+    trs = lambda stims: mult*func(stims)
+    return trs, rf_cents
+
+def get_local_manifold_rfs(trs, pt, rf_mult=1, delt=5):
+    diff = trs(pt) - trs(pt + delt)
+    norm_diff = diff/delt # /np.sqrt(np.sum(diff**2))
+    return norm_diff/rf_mult
+
+def estimate_error(local, cov):
+    est_var = np.dot(local, np.dot(cov, local))
+    return est_var
+
+def estimate_est_cov(local1, local2, cov):
+    est_cov = np.dot(local1, np.dot(cov, local2))
+    return est_cov
+
+def error_theory(trs, n, dists, rf_mult, noise_cov):
+    pairs = _make_pairs(dists, n)
+    var = np.zeros(len(dists))
+    cov = np.zeros_like(var)
+    for i, p in enumerate(pairs):
+        p1, p2 = p
+        lm1 = get_local_manifold_rfs(trs, p1, rf_mult=rf_mult)[0]
+        lm2 = get_local_manifold_rfs(trs, p2, rf_mult=rf_mult)[0]
+        var[i] = estimate_error(lm1, noise_cov)
+        cov[i] = estimate_est_cov(lm1, lm2, noise_cov)
+    return var, cov
+
+def make_covariance_matrix(size, var, corr_scale, corr_func=None):
+    cov = np.identity(size)*var
+    if corr_func is None:
+        cov[cov == 0] = corr_scale*var
+    else:
+        for c in it.combinations(range(size), 2):
+            cij = var*corr_scale*corr_func(c[0], c[1])
+            cov[c[0], c[1]] = cij
+            cov[c[1], c[0]] = cij
+    return cov
+
+def _make_pairs(dists, n):
+    pairs = np.ones((len(dists), 2))*(n/2 - max(dists)/2)
+    pairs[:, 1] = pairs[:, 1] + dists
+    return pairs
+
+def make_distance_cov_func(tau):
+    return lambda x, y: np.exp(-((x - y)**2)/tau)
+
+def estimate_encoding_swap(rf_size, rf_spacing, noise_var, noise_corr, dists,
+                           noise_samps=1000, rf_mult=10, space_size=None,
+                           buff=None, buff_mult=4, corr_func=None,
+                           distortion=nmc.mse_distortion,
                            give_real=True, basin_hop=True, parallel=False,
-                           oo=False, n_hops=100):
+                           oo=False, n_hops=100, c=1, space_mult=2):
     if buff is None:
-        buff = rf_size
-    if n is None:
-        n = np.ceil(max(dists)) + buff_mult*buff
-        n = int(n)
-    v = (snr**2)*noise_var
-    rfs, _, trs = nmc.make_code_with_power(c, o, n, v, rf_size, buff=buff,
-                                           reses=reses, rf_tiling=rf_tiling,
-                                           excl=excl, dist_samps=dist_samps,
-                                           p_measure=p_measure, neurs=neurs,
-                                           power_metric=power_metric,
-                                           object_oriented=oo)
-    d_pairs = np.ones((len(dists), 2))*(n/2 - max(dists)/2)
-    d_pairs[:, 1] = d_pairs[:, 1] + dists
-    dummy_filt = np.identity(len(rfs))
+        buff = rf_size*buff_mult
+    if space_size is None:
+        space_size = np.ceil(max(dists)) + space_mult*buff
+        space_size = int(space_size)
+    trs, cents = generate_1d_rfs(space_size, rf_spacing, rf_size, mult=rf_mult)
+    d_pairs = _make_pairs(dists, space_size)
     all_pts = d_pairs.reshape((2*len(dists), 1))
     trs_pts = trs(all_pts)
-    r_pts = trs_pts.reshape((len(dists), 2, 1, -1))
-    noise_mean = np.zeros(len(rfs))
-    noise_cov = np.identity(len(rfs))*np.sqrt(noise_var)
-    noise_cov[noise_cov == 0] = noise_var*noise_corr
+    noise_mean = np.zeros(len(cents))
+    noise_cov = make_covariance_matrix(len(cents), noise_var, noise_corr,
+                                       corr_func=corr_func)
     noise_distrib = sts.multivariate_normal(noise_mean, noise_cov)
     noise = noise_distrib.rvs((len(dists), noise_samps))
-    noise = noise.reshape((len(dists), 1, noise_samps, len(rfs)))
-    noise_pts = r_pts + noise
-    flat_shape = (len(dists)*2*noise_samps, len(rfs))
-    flat_noise_pts = noise_pts.reshape(flat_shape)
+    noise = np.expand_dims(noise, axis=1)
+    trs_pts_samp = np.repeat(trs_pts, noise_samps, axis=0)
+    trs_pts_struct = trs_pts_samp.reshape((len(dists), 2, noise_samps,
+                                           len(cents)))
+    noise_pts = trs_pts_struct + noise
+    noise_pts_flat = noise_pts.reshape((len(dists)*2*noise_samps, -1))
     all_pts_ns = np.repeat(all_pts, noise_samps, axis=0)
     if give_real:
         give_pts = all_pts_ns
     else:
         give_pts = None
-    decoded_pts = nmc.decode_pop_resp(c, flat_noise_pts, trs, n - buff, buff,
-                                      real_pts=give_pts, basin_hop=basin_hop,
-                                      parallel=parallel, niter=n_hops)
+    decoded_pts = nmc.decode_pop_resp(c, noise_pts_flat, trs,
+                                      space_size - buff,
+                                      buff, real_pts=give_pts,
+                                      basin_hop=basin_hop, parallel=parallel,
+                                      niter=n_hops)
     dist = distortion(all_pts_ns, decoded_pts, axis=1)
     struct_dist = dist.reshape(d_pairs.shape + (noise_samps,))
-    struct_decoded_pts = decoded_pts.reshape(d_pairs.shape + (noise_samps,))
-    struct_all_pts = all_pts_ns.reshape(d_pairs.shape + (noise_samps,))
+    struct_decoded_pts = decoded_pts.reshape(struct_dist.shape)
+    struct_all_pts = all_pts_ns.reshape(struct_dist.shape)
     return struct_dist, struct_decoded_pts, struct_all_pts
 
 def analyze_decoding(clean_pts, decoded_pts, distortion):
@@ -391,32 +435,36 @@ def analyze_decoding(clean_pts, decoded_pts, distortion):
     var = np.var(decoded_pts, axis=2)
     cov = np.mean((decoded_pts[:, 0] - mean_dec[:, 0])
                   *(decoded_pts[:, 1] - mean_dec[:, 1]), axis=1)
-    decoded_dists = np.diff(decoded_pts, axis=1)[:, 0, :]
+    decoded_dists = decoded_pts[:, 1, :] - decoded_pts[:, 0, :]
     switches = np.sum(decoded_dists < 0, axis=1)/decoded_pts.shape[2]
     return bias, var, cov, switches, decoded_dists, mse
 
-def characterize_encoding(snr, rf_size, noise_var, noise_corr, dists,
-                          noise_samps=1000, buff=None, n=None, buff_mult=4,
-                          reses=None, rf_tiling=None, c=1, o=1, excl=True,
-                          dist_samps=10000, p_measure=np.median, neurs=None,
-                          filt_func=nmc.pure_filter, power_metric='variance',
-                          samps=1000, distortion=nmc.mse_distortion,
-                          give_real=True, basin_hop=True, parallel=False,
-                          oo=False, n_hops=100):
+def characterize_encoding(rf_size, rf_spacing, noise_var, noise_corr, dists,
+                          noise_samps=1000, space_size=None, buff=None,
+                          buff_mult=4, distortion=nmc.mse_distortion,
+                          give_real=True, basin_hop=False, parallel=False,
+                          oo=False, n_hops=100, rf_mult=10, corr_func=None,
+                          space_mult=2):
     n_dists = len(dists)
     n_nc = len(noise_corr)
     bias = np.zeros((n_nc, n_dists, 2))
     var = np.zeros_like(bias)
+    mse = np.zeros_like(var)
     cov = np.zeros((n_nc, n_dists))
     switch = np.zeros((n_nc, n_dists))
     d_dists = np.zeros((n_nc, n_dists, noise_samps))
-
+    distorted = np.zeros((n_nc, n_dists, 2, noise_samps))
+    decoded = np.zeros_like(distorted)
+    orig = np.zeros_like(distorted)
     for i, nc in enumerate(noise_corr):
-        out = estimate_encoding_swap(snr, rf_size, noise_var, nc, dists,
+        out = estimate_encoding_swap(rf_size, rf_spacing, noise_var, nc, dists,
                                      basin_hop=basin_hop, give_real=give_real, 
-                                     noise_samps=noise_samps)
-        distortion, decoded, orig = out
-        out = analyze_decoding(orig, decoded, distortion)
-        bias[i], var[i], cov[i], switch[i], d_dists[i] = out
-    return bias, var, cov, switch, d_dists
+                                     noise_samps=noise_samps, n_hops=n_hops,
+                                     space_size=space_size, corr_func=corr_func,
+                                     rf_mult=rf_mult, distortion=distortion,
+                                     space_mult=space_mult)
+        distorted[i], decoded[i], orig[i] = out
+        out = analyze_decoding(orig[i], decoded[i], distorted[i])
+        bias[i], var[i], cov[i], switch[i], d_dists[i], mse[i] = out
+    return bias, var, cov, switch, d_dists, mse, distorted, decoded, orig
         
